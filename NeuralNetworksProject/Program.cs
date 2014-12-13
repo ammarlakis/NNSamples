24a0e3a288cee2765bb92db37077ff326bd08bbb
Ammar Lakis <ammar.lakis@gmail.com>
2014-12-13 21:58:23 +0200

    change form name

--- NeuralNetworksProject/Program.cs
+++ NeuralNetworksProject/Program.cs
@@ -12,7 +12,7 @@ namespace NeuralNetworksProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
 
         }
     }
