32def890f9e44e4285ac31aa139cfb355645403e
Ammar Lakis <ammar.lakis@gmail.com>
2014-12-23 00:40:43 +0200

    Add neural network diagram component

---
+++ NeuralNetworksProject/NetworkDiagram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeuralNetworksProject
+{
+    public partial class NetworkDiagram : UserControl
+    {
+        public NetworkDiagram()
+        {
+            InitializeComponent();
+            Graphics g = this.CreateGraphics();
+            g.DrawString("This is a test", new Font("Arial", 10), new SolidBrush(Color.Black), 20, 20);
+        }
+    }
+}
